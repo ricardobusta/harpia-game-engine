b23715d4611e506321e03c32e4b0d778d8ac9141
Ricardo Bustamante <ricardo@busta.dev>
2022-06-06 08:58:41 +0200

    Rename audio component

--- Harpia/Engine/Game/Components/AudioComponent.cpp
+++ Harpia/Engine/Game/Components/AudioComponent.cpp
@@ -2,7 +2,7 @@
 // Created by Ricardo Bustamante <ricardo@busta.dev> on 05/06/2022.
 //
 
-#include "AudioSource.h"
+#include "AudioComponent.h"
 #include "GlobalDefines.h"
 #include "Internal/Application_Internal.h"
 #include "AudioSystem.h"
---
+++ Harpia/Engine/Game/Components/AudioComponent.h
@@ -0,0 +1,26 @@
+//
+// Created by Ricardo Bustamante <ricardo@busta.dev> on 05/06/2022.
+//
+
+#ifndef HARPIAGAMEENGINE_AUDIOCOMPONENT_H
+#define HARPIAGAMEENGINE_AUDIOCOMPONENT_H
+
+#include <string>
+#include "Component.h"
+#include "GlobalDefines.h"
+
+namespace Harpia {
+    class AudioComponent : public Component {
+    private:
+        AudioAsset *audio = nullptr;
+        AudioSystem *_audioSystem = nullptr;
+    public:
+        explicit AudioComponent(const std::string &path);
+//
+//        ~AudioComponent();
+//
+//        void Play();
+    };
+} // Harpia
+
+#endif //HARPIAGAMEENGINE_AUDIOCOMPONENT_H
