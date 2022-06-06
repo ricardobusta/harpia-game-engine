3b319a534937bea1e83973f06f24caafdf5dc87c
Ricardo Bustamante <ricardo@busta.dev>
2022-06-06 09:48:42 +0200

    Add Assertion commands to debug

--- Harpia/Engine/Core/Debug.h
+++ Harpia/Engine/Core/Debug.h
@@ -6,11 +6,14 @@
 #define HARPIAGAMEENGINE_DEBUG_H
 
 #ifndef HARPIA_DEBUG
-#define DebugLog(args...)
-#define DebugLogWarning(args...)
-#define DebugLogError(args...)
+#define DebugLog(args...) (void(0))
+#define DebugLogWarning(args...) (void(0))
+#define DebugLogError(args...) (void(0))
+#define Assert(condition) (void(0))
 #else //HARPIA_DEBUG
 
+#include <string>
+#include <cassert>
 #include "String.h"
 
 #ifdef __MINGW32__
@@ -27,6 +30,13 @@ inline std::string DebugCallerName(const std::string &s) {
 #define HARPIA_CALLER __func__
 #endif //__MINGW32__
 
+#define DebugLog(args...) do{Harpia::Debug::Log(HARPIA_CALLER,args);}while(0)
+#define DebugLogWarning(args...) do{Harpia::Debug::LogWarning(HARPIA_CALLER,args);}while(0)
+#define DebugLogError(args...) do{Harpia::Debug::LogError(HARPIA_CALLER,__FILE__,__LINE__,args);}while(0)
+#define Assert(condition, args...) do{if(!(condition)){Harpia::Debug::LogError(HARPIA_CALLER,__FILE__,__LINE__,args);}\
+assert(condition);}while(0)
+#define AssertNotNull(condition, args...) Assert((condition)!=nullptr,"%s is nullptr",(#condition))
+
 namespace Harpia {
     class Debug {
     public:
@@ -34,7 +44,7 @@ namespace Harpia {
 
         static void LogWarning(const char *tag, const char *message);
 
-        static void LogError(const char *tag, const char *message);
+        static void LogError(const char *tag, const char *file, int line, const char *message);
 
         template<typename... Args>
         static void Log(const char *tag, const char *format, Args ... args) {
@@ -47,15 +57,11 @@ namespace Harpia {
         }
 
         template<typename... Args>
-        static void LogError(const char *tag, const char *format, Args ... args) {
-            LogError(tag, String::Format(format, args...).c_str());
+        static void LogError(const char *tag, const char *file, int line, const char *format, Args ... args) {
+            LogError(tag, file, line, String::Format(format, args...).c_str());
         }
     };
 }
-
-#define DebugLog(args...) do{Harpia::Debug::Log(HARPIA_CALLER,args);}while(0)
-#define DebugLogWarning(args...) do{Harpia::Debug::LogWarning(HARPIA_CALLER,args);}while(0)
-#define DebugLogError(args...) do{Harpia::Debug::LogError(HARPIA_CALLER,args);}while(0)
 
 #endif //HARPIA_DEBUG
 #endif //HARPIAGAMEENGINE_DEBUG_H
