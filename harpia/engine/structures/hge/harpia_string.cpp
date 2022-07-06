e74e74ac13da83bcaa477beb689dca8e2dc3c7e1
Ricardo Bustamante <ricardo@busta.dev>
2022-07-06 08:12:34 +0200

    temporarily add file loading method to string class.

--- harpia/engine/structures/hge/harpia_string.cpp
+++ harpia/engine/structures/hge/harpia_string.cpp
@@ -5,4 +5,20 @@
 #include "harpia_string.h"
 
 namespace Harpia {
+    std::string String::ReadFile(const std::string &path, bool * ok) {
+        std::ifstream t(path);
+
+        if(t.fail()){
+            *ok = false;
+            return {};
+        }
+
+        t.seekg(0, std::ios::end);
+        size_t size = t.tellg();
+        std::string buffer(size, ' ');
+        t.seekg(0);
+        t.read(&buffer[0], size);
+        *ok = true;
+        return buffer;
+    }
 }
