6b951979da788071d0c2a5f3e632f56f6992219b
Ricardo Bustamante <ricardo@busta.dev>
2022-07-09 16:28:24 +0200

    Add check for assets folder missing in the sample project.

--- sample_game/main.cpp
+++ sample_game/main.cpp
@@ -6,10 +6,16 @@
 
 #include "hge/application.h"
 #include "game_config.h"
+#include "hge/debug.h"
+#include <filesystem>
 
 using namespace SampleGame;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
+    if(!std::filesystem::is_directory("./assets")){
+        DebugLogError("Assets folder missing");
+        return -1;
+    }
     auto app = Harpia::Application(GameConfig::Configure);
     return app.Execute();
 }
