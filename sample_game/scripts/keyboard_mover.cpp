ab2cf0a2c1a1de8db2f9bbd77d1a7f5b477a2278
Ricardo Bustamante <ricardo@busta.dev>
2022-07-10 18:33:12 +0200

    change keyboard mover inputs.

--- sample_game/scripts/keyboard_mover.cpp
+++ sample_game/scripts/keyboard_mover.cpp
@@ -10,9 +10,9 @@ using namespace Harpia;
 namespace SampleGame {
     void KeyboardMover::Update() {
         auto dt = 0.2f;
-        auto x = Input()->GetKeyIsDown(SDLK_q) - Input()->GetKeyIsDown(SDLK_a);
-        auto y = Input()->GetKeyIsDown(SDLK_w) - Input()->GetKeyIsDown(SDLK_s);
-        auto z = Input()->GetKeyIsDown(SDLK_e) - Input()->GetKeyIsDown(SDLK_d);
+        auto x = Input()->GetKeyIsDown(SDLK_d) - Input()->GetKeyIsDown(SDLK_a);
+        auto y = Input()->GetKeyIsDown(SDLK_q) - Input()->GetKeyIsDown(SDLK_e);
+        auto z = Input()->GetKeyIsDown(SDLK_s) - Input()->GetKeyIsDown(SDLK_w);
         GetObject()->transform.SetTrMatrix(GetObject()->transform.GetTrMatrix() * Matrix::Translation({x*dt, y*dt, z*dt}));
     }
 }// namespace SampleGame
