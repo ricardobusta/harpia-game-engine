39692d11dedb081f78702e3a30fe6b4fb8798ec7
Ricardo Bustamante <ricardo@busta.dev>
2022-07-28 00:05:33 +0200

    Remove method to set the transform directly Add lazy transform update

--- harpia/engine/core/hge/transform.cpp
+++ harpia/engine/core/hge/transform.cpp
@@ -6,15 +6,13 @@
 #include "hge/harpia_math.h"
 
 namespace Harpia {
-    Transform::Transform() {
-    }
-
     Vector3 Transform::GetPosition() {
         return _position;
     }
-    
+
     void Transform::SetPosition(const Vector3 &position) {
         _position = position;
+        SetTransformDirty();
     }
 
     Vector3 Transform::GetScale() {
@@ -23,25 +21,47 @@ namespace Harpia {
 
     void Transform::SetScale(const Vector3 &scale) {
         _scale = scale;
+        SetTransformDirty();
     }
 
-    //    Quaternion Transform::GetRotation() {
-    //        return _rotation;
-    //    }
-    //
-    //    void Transform::SetRotation(const Quaternion &rotation) {
-    //        _rotation = rotation;
-    //    }
+    void Transform::SetRotation(float angle, const Vector3 &axis) {
+        _rotation = glm::toQuat(Matrix::Rotation(angle, axis)); // TODO improve this?
+        SetTransformDirty();
+    }
 
     void Transform::Rotate(float angle, const Vector3 &axis) {
-        //_rotation.Rotate(angle, axis);
+        _rotation = _rotation * glm::toQuat(Matrix::Rotation(angle, axis));
+        SetTransformDirty();
     }
 
     Matrix4x4 Transform::GetTrMatrix() {
-        return _tr;
+        if (_dirty) {
+            _cachedTr = Matrix::Translation(_position) * Matrix::Rotation(_rotation) * Matrix::Scale(_scale);
+            if (_parent != nullptr) {
+                _cachedTr = _parent->GetTrMatrix() * _cachedTr; // TODO avoid recursive call?
+            }
+            _dirty = false;
+        }
+        return _cachedTr;
     }
 
-    void Transform::SetTrMatrix(const Matrix4x4 &tr) {
-        _tr = tr;
+    void Transform::SetParent(Transform *tr) {
+        // TODO make sure parent is not a child of this object
+        if (_parent != nullptr) {
+            _parent->_children.remove(tr);
+        }
+        _parent = tr;
+        _parent->_children.push_back(this);
+        SetTransformDirty();
+    }
+
+    void Transform::SetTransformDirty() {
+        if (_dirty) {
+            return;
+        }
+        _dirty = true;
+        for (auto c: _children) {
+            c->SetTransformDirty(); // TODO avoid recursive call?
+        }
     }
 }// namespace Harpia
