26d3a4929133d53034679108c703db5e79ef2c29
Ricardo Bustamante <ricardo@busta.dev>
2022-07-31 11:25:46 +0200

    Add camera clear flag parameter and set clear type method to camera component

--- harpia/engine/rendering/hge/camera_internal.cpp
+++ harpia/engine/rendering/hge/camera_internal.cpp
@@ -5,7 +5,4 @@
 #include "hge/camera_internal.h"
 
 namespace Harpia::Internal {
-    Camera_Internal::Camera_Internal(int clearMask)
-        : _clearMask(clearMask) {
-    }
 }// namespace Harpia::Internal
