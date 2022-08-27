d00a98dd01fc29d8c8fbfdd9b09cdbb2cfe3e04f
Ricardo Bustamante <ricardo@busta.dev>
2022-08-27 10:35:54 +0200

    Add helper static values to math

--- harpia/engine/structures/hge/harpia_math.h
+++ harpia/engine/structures/hge/harpia_math.h
@@ -5,11 +5,11 @@
 #ifndef HARPIAGAMEENGINE_HARPIAMATH_H
 #define HARPIAGAMEENGINE_HARPIAMATH_H
 
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/quaternion.hpp"
 #include "glm/matrix.hpp"
 #include "glm/vec3.hpp"
 #include "glm/vec4.hpp"
-#include "glm/gtx/quaternion.hpp"
-#include "glm/gtc/matrix_transform.hpp"
 
 namespace Harpia {
     using Vector2Int = glm::vec<2, int>;
@@ -20,15 +20,15 @@ namespace Harpia {
     using Matrix4x4 = glm::mat4x4;
 
     struct Matrix {
-        static Matrix4x4 Multiply(const Matrix4x4 &m1, const Matrix4x4 &m2){
+        static Matrix4x4 Multiply(const Matrix4x4 &m1, const Matrix4x4 &m2) {
             return glm::matrixCompMult(m1, m2);
         }
 
         static Matrix4x4 Translation(const Vector3 &translation) {
-            return glm::translate(glm::identity<Matrix4x4>(),translation);
+            return glm::translate(glm::identity<Matrix4x4>(), translation);
         }
 
-        static Matrix4x4 Scale(const Vector3 &scale){
+        static Matrix4x4 Scale(const Vector3 &scale) {
             return glm::scale(glm::identity<Matrix4x4>(), scale);
         }
 
@@ -36,11 +36,11 @@ namespace Harpia {
             return glm::rotate(glm::identity<Matrix4x4>(), angle, axis);
         }
 
-        static Matrix4x4 Rotation(const Quaternion &rotation){
+        static Matrix4x4 Rotation(const Quaternion &rotation) {
             return glm::toMat4(rotation);
         }
 
-        static Matrix4x4 Identity(){
+        static Matrix4x4 Identity() {
             return glm::identity<Matrix4x4>();
         }
 
@@ -48,7 +48,7 @@ namespace Harpia {
             return glm::perspective<float>(fovy, aspect, near, far);
         }
 
-        static Matrix4x4 Orthographic(float left, float right, float top, float bottom, float near, float far){
+        static Matrix4x4 Orthographic(float left, float right, float top, float bottom, float near, float far) {
             return glm::ortho<float>(left, right, bottom, top, near, far);
         }
     };
@@ -71,7 +71,13 @@ namespace Harpia {
     struct Vector<3> {
         constexpr static const Vector3 one = Vector3(1, 1, 1);
         constexpr static const Vector3 zero = Vector3(0, 0, 0);
+        constexpr static const Vector3 up = Vector3(0, 1, 0);
+        constexpr static const Vector3 down = Vector3(0, -1, 0);
+        constexpr static const Vector3 left = Vector3(-1, 0, 0);
+        constexpr static const Vector3 right = Vector3(1, 0, 0);
+        constexpr static const Vector3 front = Vector3(0, 0, 1);
+        constexpr static const Vector3 back = Vector3(0, 0, -1);
     };
-} // Harpia
+}// namespace Harpia
 
-#endif //HARPIAGAMEENGINE_HARPIAMATH_H
+#endif//HARPIAGAMEENGINE_HARPIAMATH_H
