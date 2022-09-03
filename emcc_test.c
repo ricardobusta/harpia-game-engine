9e1debd72b95e4e702d0887f3a0065666cf4e9b7
Ricardo Bustamante <ricardo@busta.dev>
2022-09-03 21:58:17 +0200

    add tests

---
+++ emcc_test.c
@@ -0,0 +1,10 @@
+//
+// Created by Ricardo Bustamante <ricardo@busta.dev> on 03/09/2022.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("hello, world!\n");
+    return 0;
+}
---
+++ empp_test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ricardo Bustamante <ricardo@busta.dev> on 03/09/2022.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "hello, world!" << std::endl;
+    return 0;
+}
