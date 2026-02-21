0fcddbdfd33d3e92e3fb3c9c9e0ac9f10307a54c
Ricardo Bustamante <ricardo@busta.dev>
2026-02-21 17:38:08 -0300

    update submodules

--- hello_world/hello_world.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Ricardo Bustamante <ricardo@busta.dev> on 25/10/2022.
-//
-
-#include <iostream>
-
-int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
-    std::cout << "Hello World" << std::endl;
-}
