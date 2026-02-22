1d856570462ccb7060f0b86e98a53c26267c656f
Ricardo Bustamante <ricardo@busta.dev>
2026-02-22 18:09:42 -0300

    fix ui system compilation errors

--- harpia/engine/ui/hge/ui_system.cpp
+++ harpia/engine/ui/hge/ui_system.cpp
@@ -4,12 +4,13 @@
 
 #include "ui_system.h"
 #include "hge/core_system.h"
-#include <SDL.h>
+#include <SDL3/SDL.h>
 
 namespace Harpia::Internal {
     int UISystem::Initialize(GameConfiguration &configuration, Application *application, CoreSystem *coreSystem) {
         auto window = coreSystem->GetWindow();
-        auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        auto renderer = SDL_CreateRenderer(window, nullptr);
+        (void) renderer;
         return 0;
     }
     int UISystem::GetInitFlags() {
